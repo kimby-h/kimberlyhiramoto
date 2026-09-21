02136ae4cbd57f8ab769bd0f7d667fc4e58e22a2
Kimberly <hiramoto@berkeley.edu>
2026-09-21 15:24:47 -0700

    done with hw2

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -95,9 +95,174 @@ print(bool(not(True))) # False, not true means it's false.
 '''
 1. Empty data types will return as false, but if there's a space in between, that is considered a something, so it will come back as true. If it's true and false, it'll be false, but if it's true or false, it'll return as true. 
 2. I was surprised that expressions of True or False were true but expressions of True and False were false.
-3
-
+3. print(bool("skibidi toilet")) will return as True because a string of text within the boolean expression will evaluate to true.
+4. print(bool(None)) will return as False because the value inside the boolean expression has no value, which evaluates to false.
 '''
 print(bool("skibidi toilet"))
 print(bool(None))
+
+# 3.3.1
+
+print(10 + 5) # 15, + performs addition
+print(10 - 5) # 5, - performs subtraction
+print(2 * 4) # 8, * performs multiplication
+print(5 % 2) # 1, % calculates remainder of a division problem
+print(6 / 3) # 2, / performs division
+print(3 ** 2) # 9, ** turns the latter number into an exponent
+print(15 // 2) # 7, // divides the former by the latter and rounds to the nearest whole number
+
+# 3.3.2
+
+print(5 == 2) # False, == means equal to, the numbers aren't equal, thus false.
+print(10 != 10) # False, != means not equal to, the numbers are equal, thus false.
+print(2 < 5) # True, 5 is greater than 2.
+print(12 > 5) # True, 12 is greater than 5.
+print(5 <= 6) # True, 6 is greater than or equal 5.
+print(1 >= 10) # False, 1 is not greater than or equal to 10.
+
+# 3.3.3
+
+x = 5
+
+x += 5
+print(x)
+
+x -= 4
+print(x)
+
+x *= 3
+print(x)
+
+# 3.3.4
+
+'''
+1. The and operator will return True only if both operands are True.
+    bool(True and True) will return True since both operands are true.
+    bool(True and False) will return false as one of the operands are false
+
+2. The or operator returns true if at least one of the operands is true.
+    bool(True or False) will return True since one of the operands is true.
+    bool(False or False) will return false since both operands are false.
+
+3. The operator not negates the boolean value of the operands.
+    bool(not True) will return False, since negating True means False.
+    bool(not False) will return True, since negating False means True.
+
+More Questions:
+1. / means simple division, // means division and rounding the quotient to the nearest whole number.
+2. // means what I said above, and % performs division and the result is the remainder.
+3. I would use %, 
+    example: print(7 % 3) = 1
+4. Assignment operators change the value of a variable.
+'''
+
+# 3.4
+
+my_string = "hello" 
+print(my_string) # prints: hello
+print(my_string[0]) # prints: 0th letter, h
+print(my_string[1]) # prints: 1st letter, e
+print(my_string[2]) # prints: 2nd letter, l
+print(my_string[3]) # prints: 3rd letter, l
+print(my_string[4]) # prints: 4th letter, o
+print(my_string[-1]) # prints: last letter, o
+print(my_string[1:3]) # prints: index 1 and index 2, el
+print(my_string[0:5:2]) # prints: increments from 0-4 with a step of 2, hlo
+print(len(my_string)) # prints: the length of the string, 5
+print(my_string + "goodbye") # prints: hellogoodbye
+print(7 * my_string) #prints: hellohellohellohellohellohellohello (hello 7x)
+
+# 3.4.1
+
+'''
+1. slicing is when you extract a portion of a string by specifying start and end index and an optional step. For 8 and 9 we did this.
+'''
+
+name = "Oski"
+print("Hello, my name is", name) # it says Hello, my name is Oski.
+print(f"Hello, my name is {name}") # it says Hello, my name is Oski.
+# The difference between the first and second print statments is that one concatenates the string with the variable name, while 
+# the second one uses an f-string, where you prefix the string with f, and place the variable name in curled brackets.
+
+# 3.5
+
+'''
+cd
+changes directories. use it to move from one folder to another
+example: cd Desktop
+
+ls
+list. use it to return the list files and directories.
+example: ls Desktop
+
+ls -a
+this will list all files and folders in the current directory, including hidden files.
+example: ls -a Desktop
+
+mkdir
+make directory. this will make a new empty folder or directory within a command-line.
+example: mkdir Skibidi
+
+cat
+concatenate. used to read and display file contents.
+example: cat skibidi
+
+pwd
+print working directory. displays the exact folder path you are currently in inside the terminal
+example: pwd skibidi
+
+cd ..
+change directory to the parent directory. moves you up one directory level into the parent folder of your current location
+example: if in skibidi/toilet, cd.. will take me back to skibidi
+
+cd . 
+change to current directory. keeps you in your current working directory.
+example: if in skibidi/toilet, cd . will keep me there
+
+cd ~
+change directory to home folder. changes your current working directory to your user's home directory.
+example: if in kimberlyhiramoto/skibidi, cd~ takes me to kimberlyhiramoto
+
+cp
+copy. used to duplicate files and directories from one location to another
+example: cp skibidi tungtung
+
+mv
+move. moves or renames files and directories.
+example: mv skibidi tungtung
+
+rm
+remove. permanently deletes files and directories from your stystem.
+example: rm tungtung
+
+clear
+erases the visible text on your terminal screen and clears its scrollback history buffer.
+example: clear (and it will clear stuff)
+
+grep
+global regular expression print. searches for specific words, phrases, or text patterns within files or from the output of other commands.
+grep "tungtungtung sahur" skibidi toilet
+
+Three other commands:
+
+1. head: a command used to preview the first few rows of a data set. to see the first 5 rows, just say print(<file>.head())
+2. tail: the opposite of head, it's used to preview the last few rows of a dataset. to see the last 5 rows, which is the default, just 
+    say print(<file>.tail())
+3. whoami: a command that prints the username associated witht he current effective user ID. to use, just type whoami into the terminal 
+    and it will say back who you are, kimbe.
+
+2. ls will list all files and directories, while ls -a will do the same but also listing the hidden files.
+3. a hidden file is a file or folder that does not appear when you run a standard listing command, usually because its name starts with 
+    a dot (.).
+4.  1) -l is a flag for ls, it will show the long format of a file's contents, such as permissions, size, owner, and modified date. you 
+    do ls -l in command line.
+    2) -h is a flag for ls and -l, it'll show file sizes in an easy to read format. to use, type ls -l -h into the command line.
+    3) -i is a flag for rm, and it will prompt you to confirm before deleting each file. to use, type rm -i <file>
+'''
+
+# 4
+
+
+
+
 
---
+++ homework2/homework2.py
@@ -0,0 +1,78 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is the software, GitHub is a website that hosts the code, and GitBash is the terminal where you can write the code on the command line.
+
+# 2) What’s the difference between the terminal and the command line?
+# The terminal is the whole window while the command line is where you type out your code.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+# They are different types of shell languages, and handle data differently: PowerShell is object orientated and built for Windows, while 
+# Git Bash is text based and packaged with Git.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Python is a programming language, Conda is a package, and Anaconda is a software that has both Python and Conda amongst many other packages 
+# that you can download fairly easily.
+
+# 5) What is VS Code?
+# VS Code is a free and open source code editor.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a free and web-based digital notepad that lets you write, run, and see your code. The main difference between Jupyter
+# Notebook and Jupyter Lab is that Jupyter Lab lets you look at many windows/notbooks at once, while Jupyter Notebook is just the single notebook.
+
+# 7) What does ~/ mean?
+# ~/ represents your computer's Home Folder.
+
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path is the path to get to a folder starting from the root directory, while the relative path is the path to get to a folder in
+# relation to where you are currently.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# absolute: cd /"Python Decal"/course_assignments/homework2   relative: cd ../course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# since ./ is current directory, rm ./ would remove your current directory.
+
+# 12) What do the following commands do?
+# git add: recognizes the changes you want to make and adds them to a staging area
+# git commit: takes what's in the staging area and puts it into your history
+# git push: uploads the commits from the computer's repo and puts it into a remote repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . adds everything in your current directory, while git add <file> only adds what's in that file.
+
+# 14) What do "git status" and "git log -1" do?
+# git status will show you what files have been modified but not saved yet. git log -1 tells you your most recent commit.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# cloning creates a brand new local copy of a project while pulling will update an already existing local copy with the latest changes from
+# others.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# The most frustrating error that I've had in class so far has by far been the one where I ended up having to remake my SSH password because it
+# was telling me it was wrong, when in reality I just needed to change what I was typing in to fix it.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# I'm still a little confused with like what the difference between all the stuff we downloaded is, but I think I'm starting to understand
+# more a little bit.
+
+# 18) Tell me a fun fact!
+# I'm really hungry right now so I'm probably going to go to Cafe 3 once I finish this assignment.
+
+# 19) Print your favorite math expression you've learned in Python so far.
+# (Hint: Use print() and add a comment explaining what it does.)
+x = 3
+x += 5
+print(x)
+# and this should print 8. the expression += means that you add whatever x equals to what's on the right side of the equation.
+# Since x = 3 and 5 is on the right side of the equation, 3 + 5 = 8.
+
